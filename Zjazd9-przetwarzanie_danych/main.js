35284af55389800773fc09262371998963520a1f
sooonk <sonia@przenioslo.eu>
2019-01-19 09:38:07 +0100

    Objects

--- Zjazd9-przetwarzanie_danych/main.js
+++ Zjazd9-przetwarzanie_danych/main.js
@@ -1,2 +1,36 @@
 //PRZETWARZANIE DANYCH
 
+
+//sprawdzić statyczne metody przetwarzania danych
+
+const obj = {name:'Bob', age:30};
+
+     for(const prop in obj){
+        console.log(prop);
+        console.log(obj[prop]);   
+        
+     }
+
+console.log(Object.entries(obj));
+
+console.log(Object.keys(obj));
+console.log(Object.values(obj));
+
+
+//zamiana danych na string i odwrócenie operacji
+const x = {name:'Kasia', age:22};
+
+console.log(JSON.stringify(x));
+console.log(JSON.parse('{"name":"Kasia","age":22}'));
+let y = JSON.stringify(x); //dobrą praktyką jest zapisywanie takich operacji od razu do zmiennych
+console.log(JSON.parse(y));
+
+//Math
+
+console.log(Math.round(0.4));
+console.log(Math.round(0.7));
+console.log(Math.floor(0.7));
+console.log(Math.ceil(0.3));
+
+//Daty - korzystać z frameworków, np. moment.js
+
