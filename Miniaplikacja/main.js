e9ff66fdc829dc74a1bfa1c49e6049ab2cbcd68e
sooonk <sonia@przenioslo.eu>
2019-01-20 09:49:22 +0100

    Added functions to miniproject

--- Miniaplikacja/main.js
+++ Miniaplikacja/main.js
@@ -53,56 +53,83 @@ document.addEventListener("DOMContentLoaded", function(event) {  //Będzie się 
     };
     const heroes = [ciri, franciska, keira, philippa, tissaia, triss, yen, emhyr, dandelion, vernon, vesemir, foltest, geralt];
     console.log("Page is ready");
-});
-//Stan aplikacji powinien być zapisywany i przechowywany (dane)
-const store = {
-    heroes: [],
-    addHero: function(hero){
-        this.heroes.push(hero);
+    //Stan aplikacji powinien być zapisywany i przechowywany (dane)
+    const store = {
+        heroes: [],
+        addHero: function(hero){
+            this.heroes.push(hero);
+    
+            this.doLogState('Hero has been added: ', hero);
+        },
+       
+        removeHero: function(hero){
+            this.heroes = this.heroes
+                .filter(storedHero => storedHero.name !== hero.name);
+    
+            this.doLogState('Hero has been removed: ', hero);
+        },
+        //odwołuje się do zmiennej, która znajduje się w storze, a nie poza nim
+        getHeroes: function(){
+            return this.heroes; 
+        },
+    
+        synchToLocalStorage: function(){
+            localStorage.clear();
+    
+            this.heroes.forEach(hero => {
+                localStorage.setItem(hero.name, JSON.stringify(hero));
+            });
+            console.warn('Store: local storage state is like: ', localStorage);
+        },
+    
+        synchFromLocalStorage: function(knownHeroes){
+            knownHeroes.forEach(knownHero => {
+                const storedHero = localStorage.getItem(knownHero.name);
+    
+                if((storedHero !== null) && (storedHero !== undefined)){
+                        this.heroes.push(storedHero);
+                    } 
+            });
+            console.warn('Store: state of store after synch is like: ', this.heroes); 
+        }, 
+    
+        doLogState: function(message, hero){
+            console.warn('Store: '+ message, hero);
+            console.warn('Store: state of store is like: ', this.heroes); 
+        }    
+    };
+    /*
+    store.addHero(triss);
+    
+    store.addHero(ciri);
+    store.synchFromLocalStorage(heroes);
+    console.log(store.getHeroes());
 
-        this.doLogState('Hero has been added: ', hero);
-    },
-
-    removeHero: function(hero){
-        this.heroes = this.heroes
-            .filter(storedHero => storedHero.name !== hero.name);
-
-        this.doLogState('Hero has been removed: ', hero);
-    },
-
-    getHeroes: function(){
-        return this.heroes; //odwołuje się do zmiennej, która znajduje się w storze, a nie poza nim
-    },
-
-    synchToLocalStorage: function(hero){
-        localStorage.clear();
-
-        this.heroes.forEach(hero => {
-            localStorage.setItem(hero.name, JSON.stringify(hero));
-        });
-        console.warn('Store: local storage state is like: ', localStorage);
-    },
-
-    synchFromLocalStorage: function(knownHeroes){
-        knownHeroes.forEach(knownHero => {
-            const storedHero = localStorage.getItem(knownHero.name);
-
-            if((storedHero !== null) && (storedHero !== undefined)){
-                    this.heroes.push(storedHero);
-                } 
-        });
-
-        console.warn('Store: state of store after synch is like: ', this.heroes); 
-    }, 
-
-    doLogState: function(message, hero){
-        console.warn('Store: '+ message, hero);
-        console.warn('Store: state of store is like: ', this.heroes); 
+    store.removeHero(triss);
+    console.log(store.getHeroes());
+    */
+    
+    //Możliwość dodania bohatera
+    function buildOptionTag(hero){
+        return `<option value="${hero.name}">${hero.name}</option>`;
     }
-};
-console.log('ok');
 
-//Możliwość dodania bohatera
+    function buildOptions(heroes){
+        let options = '';
+
+        heroes.forEach(hero => {
+            options += buildOptionTag(hero);
+        });   
+        return options;
+    }
+    function appendOptions(heroes){
+        document.querySelector('#heroesSelect').innerHTML = buildOptions(heroes);       
+    }  
+    appendOptions(heroes);
+    
+});
+
+
 //Podgląd bohatera powinien pokazywać jego zdjęcie, opis i moc
 
 
