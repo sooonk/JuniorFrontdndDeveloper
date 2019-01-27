dcfb15f23aa79b519d124ddd76423489d94b6c3f
sooonk <sonia@przenioslo.eu>
2019-01-27 09:41:05 +0100

    todoApp - first steps

--- Zjazd10_JS-OOP/Aplikacja-OOP/main.js
+++ Zjazd10_JS-OOP/Aplikacja-OOP/main.js
@@ -12,24 +12,51 @@
 
 
 function ToDoApp() {
-    this.tasks = [];
-    this.tasksDone = [];
-
-    this.inputElement = (function() {
-        let input = document.createElement('input');
+    this.tasksToDo = []; // lista zadan do zrobienia
+    this.tasksDone = []; // lista zadań zrobionych
+    this.inputNode = (() => {
+        const input = document.createElement('input');
         input.type = 'text';
         return input;
-    })()//metoda zwracająca element input - wywołanie funkcji natychmiastowej wymaga dodatkowych nawiasów na końcu, aby ją wywołać
+    })() // wywołanie funkcji natychmiastowej - tworzymy znacznik <input> który będziemy trzymać w tym polu w celu optymalizacji i łatwego dostępu
+    this.listNode = document.createElement('ul');
+    this.buttonNode = (() => {
+        const button = document.createElement('button');
+        button.innerText = 'add task';
+        return button;
+    })() // wywołanie funkcji natychmiastowej
 };
 
-ToDoApp.prototype.addTaskToDo = function (event) {
+ToDoApp.prototype.renderUI = function() {
+    const appContainer = document.querySelector('#toDoAppContainer');
+    appContainer.appendChild(this.inputNode);
+    appContainer.appendChild(this.buttonNode);
+    appContainer.appendChild(this.listNode);
+    this.buttonNode.addEventListener('click', this.addTaskToDo);
+}
+
+ToDoApp.prototype.addTaskToDo = function() {
+    const inputValue = this.inputNode.value;
+    const newTask = new Task(inputValue);
+    this.tasksToDo.push(newTask);
+    
+    const newListElement = this.createNewListElement(newTask.text)
+    this.listNode.appendChild(newListElement);
+    this.inputNode.value = '';
+}
+
+ToDoApp.prototype.createNewListElement = function(text) {
+    const newListItem = document.createElement('li');
+    newListItem.innerText = text;
+    return newListItem;
 }
 
 function Task(text) {
     this.text = text;
 }
 
+const ToDoListApp = new ToDoApp();
+ToDoListApp.addTaskToDo = ToDoApp.prototype.addTaskToDo.bind(ToDoListApp)
+ToDoListApp.renderUI();
 
-const apka = new ToDoApp();
-console.log(apka);
 
